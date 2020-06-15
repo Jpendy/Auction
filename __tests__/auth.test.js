85a9e94c1160ef4af93f0c6c7089df9bc9239b30
Jpendy <Jpendy256@gmail.com>
2020-06-15 14:20:53 -0700

    made and passed test for sign up user auth route

---
+++ __tests__/auth.test.js
@@ -0,0 +1,41 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('auction routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+
+  it('it signs up a user with a POST /signup', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'jake@jake.com',
+        password: 'thisIsPassword'
+      })
+      .then(res =>
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          email: 'jake@jake.com'
+        })
+      );
+  });
+
+});
+
