5e059b175f8433abe71ec00752c70ead870cc243
Jpendy <Jpendy256@gmail.com>
2020-06-15 13:56:51 -0700

    commit

---
+++ __tests__/auctions.test.js
@@ -0,0 +1,38 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('auction routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+
+  it('it creates a new auction', () => {
+
+    return request(app)
+      .post('/api/v1/auctions')
+      .send({})
+      .then(res => {
+        expect(res.body).toEqual({
+              
+        });
+      });
+  });
+});
+
+
