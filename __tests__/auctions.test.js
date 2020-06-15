3d03efc93362e51d8783a88d35b1613ecee80a66
Jpendy <Jpendy256@gmail.com>
2020-06-15 15:32:33 -0700

    added test for get all auctions

--- __tests__/auctions.test.js
+++ __tests__/auctions.test.js
@@ -2,10 +2,13 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongod = new MongoMemoryServer();
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
-const User = require('../lib/models/User');
 
 const request = require('supertest');
 const app = require('../lib/app');
+
+const User = require('../lib/models/User');
+const Auction = require('../lib/models/Auction');
+const Bid = require('../lib/models/Bid');
 
 describe('auction routes', () => {
   beforeAll(async() => {
@@ -23,10 +26,29 @@ describe('auction routes', () => {
   });
 
   let user;
+  let auction;
+  let bid;
+
   beforeEach(async() => {
     user = await User.create({
       email: 'jake@jake.com',
       password: 'thisIsPassword'
+    });
+
+    auction = await Auction.create({
+      user: user.id,
+      title: 'auction 1',
+      description: 'this is auction 1',
+      quantity: 5,
+      endDate: Date.now()  
+    });
+
+    bid = await Bid.create({
+      auction: auction.id,
+      user: user.id,
+      price: '$5',
+      quantity: 5,
+      accepted: true  
     });
   });
 
@@ -50,9 +72,54 @@ describe('auction routes', () => {
           description: 'this is auction 1',
           quantity: 5,
           endDate: expect.any(String),
-          __v: 0  
         });
       });
+  });
+
+  it('it gets an auction by id and populates the user and lists all bids', () => {
+      
+    return request(app)
+      .get(`/api/v1/auctions/${auction._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),  
+          user: user.id,
+          title: 'auction 1',
+          description: 'this is auction 1',
+          quantity: 5,
+          endDate: expect.any(String),
+          user: { 
+            _id: user.id,
+            email: 'jake@jake.com',
+          },
+          bids: [{
+            _id: bid.id,
+            accepted: true,
+            auction: auction.id,
+            price: '$5',
+            quantity: 5,
+            user: user.id,
+            __v: 0
+          }]  
+        });
+      });
+  });
+
+  it('it gets all auctions with GET', () => {
+      
+    return request(app)
+      .get('/api/v1/auctions')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: expect.anything(),  
+          user: user.id,
+          title: 'auction 1',
+          description: 'this is auction 1',
+          quantity: 5,
+          endDate: expect.any(String),
+        }]);
+      });
+
   });
 });
 
